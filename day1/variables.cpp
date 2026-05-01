710fcf801299a342fff2c017fb16cf19b193de94
Nakshatra Garg <nakshatra@kugelblitz.xyz>
2026-05-01 15:48:16 +0530

    some unsigned ints

--- day1/variables.cpp
+++ day1/variables.cpp
@@ -8,11 +8,17 @@ int main(){
     float d = 4.3;
     double e = 4.322;
 
+    int neg = -3;
+
+
+    unsigned int ui = -122;
+    cout << ui << endl;
     cout << a << endl;
     cout << b << endl;
     cout << c << endl;
     cout << d << endl;
     cout << e << endl;
+    cout << neg << endl;
 
     int size = sizeof(a);
     cout << size << endl;

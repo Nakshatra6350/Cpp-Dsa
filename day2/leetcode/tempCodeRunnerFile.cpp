bbb69f932988b12ba832e32f13796750446fae24
Nakshatra Garg <nakshatra@kugelblitz.xyz>
2026-05-02 16:02:42 +0530

    some leetcode questions on bitwise operator

---
+++ day2/leetcode/tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+if( (ans > INT_MAX / 10) || ( ans < INTMAX_MIN / 10 ) ){ // exceptional case when ans * 10 is greater/smaller than integer range
+        //     ans = 0;
+        //     break;
+        // }
